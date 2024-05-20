6ef8ecddba8485603da427e86004b8ed0cbb1d50
JamJestJerzy <github@j3rzy.dev>
2024-05-20 12:23:01 +0200

    Removed junk

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,12 @@
 import "@/app/globals.css";
 
 import inter from "@/lib/fonts";
+import { cn } from "@/lib/utils";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, "")}>{children}</body>
     </html>
   );
 }
