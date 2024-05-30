7d3558b7fc3a227d3a8a4b786e1c3a2a527af1ae
JamJestJerzy <github@j3rzy.dev>
2024-05-31 00:36:43 +0200

    Added saving dark theme state

--- providers/dark-mode.js
+++ providers/dark-mode.js
@@ -1,13 +1,32 @@
 "use client";
 
 import { DarkModeContext } from "@/contexts/dark-mode";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const DarkModeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  /*
+    Ik that it throws
+    ```
+     ⨯ providers\dark-mode.js (7:48) @ localStorage
+     ⨯ ReferenceError: localStorage is not defined
+      at DarkModeProvider (./providers/dark-mode.js:14:89)
+      digest: "2766037793"
+     5 |
+     6 | export const DarkModeProvider = ({ children }) => {
+  >  7 |   const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem("dark-mode") === "true");
+       |                                                ^
+     8 |
+     9 |   const toggleDarkMode = () => {
+    10 |     setIsDarkMode(prevMode => !prevMode);
+    GET / 500 in 328ms
+  ```
+  but it works
+   */
+  const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem("dark-mode") === "true");
 
   const toggleDarkMode = () => {
     setIsDarkMode(prevMode => !prevMode);
+    localStorage.setItem("dark-mode", `${!isDarkMode}`);
   };
 
   return (
