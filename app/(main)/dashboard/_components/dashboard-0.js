479f79d58e485407559e353611b74e10e13a0e4f
JamJestJerzy <github@j3rzy.dev>
2024-06-02 20:07:34 +0200

    Moved Card header and footer to it's file

--- app/(main)/dashboard/_components/dashboard-0.js
+++ app/(main)/dashboard/_components/dashboard-0.js
@@ -1,18 +1,19 @@
 "use client";
 
-import { Card } from "@/components/card";
+import { Card, CardFooter, CardHeader } from "@/components/card";
 import AtWork from "@/public/at-work.svg";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { CircularButton } from "@/components/circular-button";
 import { DotsVerticalIcon } from "@radix-ui/react-icons";
 import ProgressBar from "@/components/progress-bar";
+import Link from "next/link";
 
 export default function Page ()
 {
   const progress = [
-    { name: "Server Migration", percentage: 20, color: "red"},
-    { name: "Sales Tracking", percentage: 40, color: "yellow"},
+    { name: "Server Migration", percentage: 20, color: "red" },
+    { name: "Sales Tracking", percentage: 40, color: "yellow" },
     { name: "Customer Database", percentage: 60, color: "blue" },
     { name: "Payout Details", percentage: 80, color: "cyan" },
     { name: "Account Setup", percentage: 100, color: "green" },
@@ -21,15 +22,15 @@ export default function Page ()
   return (
     <main className="relative overflow-x-hidden h-full w-full bg-background dark:bg-background-dark transition-all">
       <header
-        className="absolute top-0 left-0 right-0 bg-gradient-to-r from-primary to-secondary py-10 h-64 z-0 w-full"
+        className="top-0 left-0 right-0 bg-gradient-to-r from-primary to-secondary py-10 h-64 z-0 w-full"
       />
-      <article className="relative z-10 w-full overflow-x-hidden px-6 xl:px-[5vw] h-full">
-        <div className="relative flex flex-col 2xl+:flex-row 2xl+:space-x-6 max-2xl+:space-y-6 w-full top-40">
+      <article className="z-10 w-full overflow-x-hidden px-6 xl:px-[5vw] -mt-20 pb-6">
+        <div className="relative flex flex-col 2xl+:flex-row 2xl+:space-x-6 max-2xl+:space-y-6 w-full">
           <Card
             className={cn(
               "p-12 w-full 2xl+:w-[33%] flex items-center",
               "max-xl:flex-col xl:flex-row 2xl+:flex-col",
-              "max-xl:justify-normal xl:justify-between 2xl+:justify-normal"
+              "max-xl:justify-normal xl:justify-between 2xl+:justify-normal",
             )}
           >
             <div className="max-xl:text-center 2xl+:text-center">
@@ -57,22 +58,24 @@ export default function Page ()
               </div>
             </Card>
             <Card className="w-full 2xl+:w-[50%] p-5">
-              <div className={cn(
-                "h-14 -m-5 mb-5 w-[calc(100%+2.5rem)]",
-                "bg-gray-300/50 dark:bg-gray-900/25 border-gray-300 dark:border-gray-900/75",
-                "rounded-t-md border-b-[1px]",
-                "flex items-center justify-between px-5",
-              )}>
-                <p className="text-lg text-primary-lighter">Progress Tracker</p>
-                <CircularButton>
-                  <DotsVerticalIcon />
-                </CircularButton>
-              </div>
-              <div className="flex flex-col space-y-4">
+              <CardHeader>
+                <div className="flex items-center justify-between px-5 w-full">
+                  <p className="text-lg text-primary-lighter">Progress Tracker</p>
+                  <CircularButton>
+                    <DotsVerticalIcon />
+                  </CircularButton>
+                </div>
+              </CardHeader>
+
+              <div className="flex flex-col space-y-4 mb-10">
                 {progress.map( ( progress, index ) => (
                   <ProgressBar key={index} progress={progress} index={index} />
-                ))}
+                ) )}
               </div>
+
+              <CardFooter>
+
+              </CardFooter>
             </Card>
           </div>
         </div>
--- components/card.js
+++ components/card.js
@@ -1,7 +1,33 @@
 import { cn } from "@/lib/utils";
+import { CircularButton } from "@/components/circular-button";
+import { DotsVerticalIcon } from "@radix-ui/react-icons";
 
 export const Card = ( { children, className, ...props } ) => (
   <div className={cn( "bg-white dark:bg-gray-800 shadow-md rounded-md p-4", className )} {...props}>
     {children}
   </div>
 );
+
+export const CardHeader = ( { children, className } ) => (
+  <div className={cn(
+    "h-14 -m-5 mb-5 w-[calc(100%+2.5rem)]",
+    "bg-gray-300/50 dark:bg-gray-900/25 border-gray-300 dark:border-gray-900/75",
+    "rounded-t-md border-b-[1px]",
+    "flex items-center",
+    className
+  )}>
+    {children}
+  </div>
+)
+
+export const CardFooter = ( { children, className } ) => (
+  <div className={cn(
+    "h-14 -m-5 w-[calc(100%+2.5rem)]",
+    "bg-gray-300/50 dark:bg-gray-900/25 border-gray-300 dark:border-gray-900/75",
+    "rounded-b-md border-t-[1px]",
+    "flex items-center",
+    className
+  )}>
+    {children}
+  </div>
+)
