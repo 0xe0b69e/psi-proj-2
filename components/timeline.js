127fc6d7ab7333bdd8407d4a138de8d616280546
JamJestJerzy <github@j3rzy.dev>
2024-06-04 14:01:14 +0200

    Fixed timeline

--- components/timeline.js
+++ components/timeline.js
@@ -13,58 +13,61 @@ export default function Timeline ( { points = [] } )
 {
   const [ isVisible, setIsVisible ] = useState( false );
 
+  const isFirst = ( index ) => index === 0;
+  const isLast = ( index ) => index === points.length - 1;
+
   return (
     <Intersecting
       onVisible={() => setIsVisible( true )}
       onHide={() => setIsVisible( false )}
     >
-      <div className="flex flex-row w-full space-x-5">
-        <div className="flex flex-col items-end space-y-8 w-12">
-          {points.map( ( { date }, index ) => (
-            <p
-              key={index}
-              className={cn(
-                "text-xs text-slate-500 transition-all duration-1000 ease-in-out",
-                isVisible ? "translate-x-0 opacity-100" : "translate-x-9 opacity-0"
-              )}
-              style={{ transitionDelay: `${( index + 1 ) * 0.15}s` }}
-            >
-              {shortRelativeTime( date )}
-            </p>
-          ) )}
-        </div>
-        <div
-          className={cn(
-            "flex flex-col w-1 bg-gray-100 dark:bg-slate-700/25 items-center pt-0.5",
-            isVisible && "space-y-[2.39rem]"
-          )}
-        >
-          {points.map( ( { color }, index ) => (
-            <span
-              key={index}
-              className={cn(
-                "rounded-full transition-all duration-1000 ease-in-out w-2.5 h-2.5",
-                isVisible ? "opacity-100" : "opacity-0"
-              )}
-              style={{ background: color }}
-            />
-          ) )}
-        </div>
-        <div className="flex flex-col items-start space-y-8">
-          {points.map( ( { name }, index ) => (
-            <p
-              key={index}
-              className={cn(
-                "text-xs text-slate-500 transition-all duration-1000 ease-in-out transform",
-                isVisible ? "-translate-x-0 opacity-100" : "-translate-x-10 opacity-0"
-              )}
-              style={{ transitionDelay: `${index * 0.30}s` }}
-            >
-              {name}
-            </p>
-          ) )}
-        </div>
-      </div>
+      <table>
+        <tbody>
+        {points.map( ( { color, name, date }, index ) => (
+          <tr key={index} className="text-xs">
+            <td className="p-2 items-center">
+              <p
+                className={cn(
+                  "text-end text-nowrap transition-all duration-1000 ease-in-out",
+                  isVisible ? "translate-x-0 opacity-100" : "translate-x-10 opacity-0"
+                )}
+                style={{ transitionDelay: `${( index + 1 ) * 0.1}s` }}
+              >
+                {shortRelativeTime( date )}
+              </p>
+            </td>
+            <td className="p-2 h-full items-center relative">
+              <div
+                className={cn(
+                  "w-2.5 h-2.5 rounded-full transition-all duration-1000 ease-in-out z-20 relative",
+                  isVisible ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0"
+                )}
+                style={{ background: color, transitionDelay: `${( index + 1 ) * 0.05}s` }}
+              />
+              <div
+                className="w-1 h-14 bg-gray-100 dark:bg-foreground-dark absolute z-10 rounded-md"
+                style={{
+                  top: `${isLast( index ) ? 10 : isFirst( index ) ? 90 : 50}%`,
+                  left: "50%",
+                  transform: "translate(-50%, -50%)"
+                }}
+              />
+            </td>
+            <td className="p-2">
+              <p
+                className={cn(
+                  "transition-all duration-1000 ease-in-out",
+                  isVisible ? "translate-x-0 opacity-100" : "-translate-x-10 opacity-0"
+                )}
+                style={{ transitionDelay: `${( index + 1 ) * 0.15}s` }}
+              >
+                {name}
+              </p>
+            </td>
+          </tr>
+        ) )}
+        </tbody>
+      </table>
     </Intersecting>
   );
 }
