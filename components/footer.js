6fac6a79865fc326b8e59af69e24b1f284caa8fd
JamJestJerzy <github@j3rzy.dev>
2024-06-08 22:26:38 +0200

    Added /credits to footer

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,15 @@
 import Link from "next/link";
 
 export const Footer = () => (
-  <footer className="bottom-0 p-10 justify-between flex bg-background dark:bg-background-dark border-primary border-t-2">
+  <footer className="bottom-0 p-5 px-8 justify-between flex bg-background dark:bg-background-dark border-primary border-t-2 items-center">
     <p>Copyright © Someone Somewhere</p>
-    <Link href="/tos">
-      <p>ToS</p>
-    </Link>
+    <div className="flex flex-col items-center text-sky-500">
+      <Link href="/tos" className="hover:underline">
+        <p>ToS</p>
+      </Link>
+      <Link href="/credits" className="hover:underline">
+        <p>Credits</p>
+      </Link>
+    </div>
   </footer>
 )
